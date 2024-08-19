4e3a747fd26153c6e316186d43559fd08498daf0
Architgupta07 <gupta.archit01@gmail.com>
2024-08-19 22:39:57 +0530

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://fee-scheduling-be.vercel.app';
-// const BASE_URL = 'http://127.0.0.1:8000';
+// const BASE_URL = 'https://fee-scheduling-be.vercel.app';
+const BASE_URL = 'http://127.0.0.1:8000';
 
 
 
